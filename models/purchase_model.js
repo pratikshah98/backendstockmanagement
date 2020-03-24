595e2dc2182dbd467b1365fa56c4da46aaa2d9af
prachi <prachisoni24599@gmail.com>
2020-03-24 16:13:35 +0530

    purchase changes done

--- models/purchase_model.js
+++ models/purchase_model.js
@@ -4,7 +4,7 @@ var purchase={
         return db.query('select * from purchase',callback);
     },
     addPurchase:function(item,callback){
-        return db.query("insert into purchase(purchaseDate,fkSupplierEmailId,fkBranchId) values(?,?,?)",[item.purchaseDate,item.fkSupplierEmailId,item.fkBranchId],callback);
+        return db.query("insert into purchase(purchaseId,purchaseDate,fkSupplierEmailId,fkBranchId) values(?,?,?,?)",[item.purchaseId,item.purchaseDate,item.fkSupplierEmailId,item.fkBranchId],callback);
     },
     deletePurchase:function(id,callback){
         return db.query("delete from purchase where purchaseId=?",[id],callback);
@@ -17,10 +17,10 @@ var purchase={
 
         return db.query("update purchase set purchaseDate=?,fkSupplierEmailId=?,fkBranchId=? where purchaseId=?",[item.purchaseDate,item.fkSupplierEmailId,item.fkBranchId,id],callback);
     },
-    getPurchaseId:function(item,callback)
-    {
-        return db.query("select purchaseId from purchase where purchaseDate=? and fkSupplierEmailId=? and fkBranchId=?",[item.purchaseDate,item.fkSupplierEmailId,item.fkBranchId],callback);
-    }
+    // getPurchaseId:function(item,callback)
+    // {
+    //     return db.query("select purchaseId from purchase where purchaseDate=? and fkSupplierEmailId=? and fkBranchId=?",[item.purchaseDate,item.fkSupplierEmailId,item.fkBranchId],callback);
+    // }
 
 };
 module.exports=purchase;
