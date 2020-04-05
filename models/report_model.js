84f11c68c5b729948aedd700c6682ce76e4d9fa1
prachi <prachisoni24599@gmail.com>
2020-04-05 14:41:00 +0530

    report changes done

--- models/report_model.js
+++ models/report_model.js
@@ -12,9 +12,9 @@ getSaleReportBetweenDatePassedBranchId:function(sd,ed,bid,callback){
      
 },
 getSaleReportBetweenDatePassed:function(sd,ed,callback){
-    return db.query("select f.name, SUM(f.saleQuantity) as Quantity from "
-    +"(select i.name,sd.saleQuantity,s.fkBranchId from salesdetails as sd "+
-    " join sale as s on s.saleId=sd.fkSaleId join item as i on i.itemId=sd.fkItemId"+
+    return db.query("select f.name, SUM(f.saleQuantity) as Quantity,f.branchName from "
+    +"(select i.name,sd.saleQuantity,s.fkBranchId,b.branchName from salesdetails as sd "+
+    " join sale as s on s.saleId=sd.fkSaleId join item as i on i.itemId=sd.fkItemId join branch as b on b.branchId=s.fkbranchId"+
     " where s.salesDate BETWEEN ? and ?) as f group by(f.name)",[sd,ed],callback);
      
 },
@@ -30,16 +30,16 @@ getPurchaseReportBetweenDatePassedBranchId:function(sd,ed,bid,callback){
 getPurchaseReportBetweenDatePassed:function(sd,ed,callback){
     
 
-    return db.query("select f.name, SUM(f.purchaseQuantity) as Quantity from"+
-    " (select i.name,pd.purchaseQuantity,p.fkBranchId from purchasedetails as pd "+
-    "join purchase as p on p.purchaseId=pd.fkpurchaseId join item as i on i.itemId=pd.fkItemId"+
+    return db.query("select f.name, SUM(f.purchaseQuantity) as Quantity,f.branchName from"+
+    " (select i.name,pd.purchaseQuantity,p.fkBranchId,b.branchName from purchasedetails as pd "+
+    "join purchase as p on p.purchaseId=pd.fkpurchaseId join item as i on i.itemId=pd.fkItemId join branch as b on b.branchId=p.fkBranchId"+
     " where p.purchaseDate BETWEEN ? and ?) as f group by(f.name)",[sd,ed],callback);
      
 },
 getSaleReportByDateAndItemPassedBranchId:function(iid,sd,ed,bid,callback){
     
 
-    return db.query("select f.name,f.salesDate,sum(f.saleQuantity) from"+
+    return db.query("select f.name,f.salesDate,sum(f.saleQuantity) as Quantity from"+
     " (select i.name,s.salesDate,sd.saleQuantity from salesdetails as sd "+
     "join sale as s on s.saleId=sd.fkSaleId join item as i on i.itemId=sd.fkItemId"+
     " where s.fkBranchId=? and sd.fkItemId=? and s.salesDate BETWEEN ? and ?) as f"+
@@ -49,9 +49,9 @@ getSaleReportByDateAndItemPassedBranchId:function(iid,sd,ed,bid,callback){
 getSaleReportByDateAndItemPassed:function(iid,sd,ed,callback){
     
 
-    return db.query("select i.name,s.salesDate,sum(sd.saleQuantity)"+
+    return db.query("select i.name,s.salesDate,sum(sd.saleQuantity) as Quantity,b.*"+
     " from salesdetails as sd join sale as s on s.saleId=sd.fkSaleId "+
-    "join item as i on i.itemId=sd.fkItemId where sd.fkItemId=? and s.salesDate BETWEEN ? and ? "+
+    "join item as i on i.itemId=sd.fkItemId join branch as b on b.branchId=s.fkBranchId where sd.fkItemId=? and s.salesDate BETWEEN ? and ? "+
     "group by(s.salesDate)",[iid,sd,ed],callback);
      
 },
@@ -66,17 +66,17 @@ getPurchaseReportByDateAndItemPassedBranchId:function(itemId,to,from,branchId,ca
 },
 getPurchaseReportByDateAndItemPassed:function(itemId,to,from,callback)
 {
-  return db.query("select i.name, sum(pd.purchaseQuantity) as 'purchase' "+
+  return db.query("select i.name, sum(pd.purchaseQuantity) as 'purchase',b.* "+
   ",DATE_FORMAT(p.purchasedate, '%d %m %Y') as 'date'"+
   " from purchasedetails pd join purchase p on p.purchaseId = pd.fkPurchaseId "+
-  "join item i on i.itemId = pd.fkItemId  where fkItemId = ? and p.purchaseDate between ? and ?"+
+  "join item i on i.itemId = pd.fkItemId join branch as b on b.branchId=p.fkBranchId where fkItemId = ? and p.purchaseDate between ? and ?"+
   " group by p.purchasedate",[itemId,to,from],callback);       
 },
 getSaleDetailsByPassingMonth:function(itemId,callback)
       {
-          return db.query("select i.name,sum(sd.saleQuantity) as quantity,"+
-          "DATE_FORMAT(sa.salesDate,'%m - %Y') as date from salesdetails sd ,sale sa,item i "+
-          "where sd.fkSaleId = sa.saleId and sd.fkItemId=i.itemId and i.itemId = ?  and "+
+          return db.query("select i.name,b.*,sum(sd.saleQuantity) as quantity,"+
+          "DATE_FORMAT(sa.salesDate,'%m - %Y') as date from salesdetails sd ,branch b,sale sa,item i "+
+          "where b.branchId=sa.fkBranchId and sd.fkSaleId = sa.saleId and sd.fkItemId=i.itemId and i.itemId = ?  and "+
           " MONTH(sa.salesDate) = MONTH(CURRENT_DATE())"+
           "AND YEAR(sa.salesDate) = YEAR(CURRENT_DATE()) group by sd.fkitemId",[itemId],callback);
     },
@@ -90,9 +90,9 @@ getSaleDetailsByPassingMonthAndBranchId:function(itemId,branchId,callback)
     },
     getPurchaseDetailsMonthWise:function(itemId,callback)
     {
-     return db.query("select i.name,sum(pd.purchaseQuantity) as quantity,"+
-     "DATE_FORMAT(pu.purchaseDate,'%m - %y') as date from purchasedetails pd ,purchase pu,item i"+
-     " where pd.fkPurchaseId = pu.purchaseId and i.itemId=pd.fkItemId and i.itemId = ?"+
+     return db.query("select i.name,b.*,sum(pd.purchaseQuantity) as quantity,"+
+     "DATE_FORMAT(pu.purchaseDate,'%m - %y') as date from purchasedetails pd ,branch b,purchase pu,item i"+
+     " where b.branchId=pu.fkBranchId and pd.fkPurchaseId = pu.purchaseId and i.itemId=pd.fkItemId and i.itemId = ?"+
      " and MONTH(pu.purchaseDate)=MONTH(CURRENT_DATE())"+
      " AND YEAR(pu.purchaseDate)= YEAR(CURRENT_DATE()) group by pd.fkItemId",[itemId],callback);       
   },
