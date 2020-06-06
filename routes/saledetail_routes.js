c23374be5be4a09b66a0d6012c8f817af5643dd9
Sunil Keswani <keswanisunil98@gmail.com>
2020-06-06 08:15:53 +0530

    stock change on purchase/sale add/edit/del

--- routes/saledetail_routes.js
+++ routes/saledetail_routes.js
@@ -23,75 +23,111 @@ router.get("/:sid", function (req, res, next) {
 });
 
 router.delete("/:sid", function (req, res, next) {
-  saledetail.deleteSalesdetail(req.params.sid, function (err, rows) {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(rows);
+  console.log("Inside 1");
+    db.query("select * from sale where saleId=?",[req.params.sid],function (err, result, fields) {
+      console.log("Inside 2");
+        if(result){
+          console.log("Inside 3");
+          console.log(req.params.sid);
+          db.query("select * from salesdetails where fkSaleId = ?",[req.params.sid],function(err1,result1,fields1){
+            if(result1){
+              console.log("Inside 4");
+              console.log(result1.length);
+              let myBranch=result[0].fkBranchId;
+              let stockUsage=[];
+              let promise = new Promise((resolve,reject)=>{
+                let i=0;
+                console.log(result1.length);
+                  for(i=0;i<result1.length;i++){
+                    console.log("Adding Stock= "+parseInt(result1[i].saleQuantity));
+                    stockUsage.push({
+                      itemId:result1[i].fkItemId,
+                      branchId:myBranch,
+                      stock:parseInt(result1[i].saleQuantity)
+                    });  
+                  }
+                  if(i==result1.length){
+                    console.log("Inside 5");
+                    resolve('Done');
+                  }
+              });
+              promise.then((resolve)=>{
+                if(resolve=='Done'){
+                  console.log("Inside 6");
+                  db.query("select * from stock where fkBranchId = ?",myBranch,function(err2,result2,fields2){
+                    if(result2){
+                      console.log("Inside 7");
+                      let promise2 = new Promise((resolve2,reject2)=>{
+                        console.log("Inside 8");
+                        let j=0;
+                        for(j=0;j<result2.length;j++){
+                          for(let z=0;z<stockUsage.length;z++){
+                              if(result2[j].fkItemId==stockUsage[z].itemId){
+                                stockUsage[z].stock=parseInt(result2[j].stockQuantity) + parseInt(stockUsage[z].stock); 
+                                console.log("Stock 1 ="+parseInt(stockUsage[z].stock)+"   "+parseInt(result2[j].stockQuantity));
+                              }
+                              // console.log("Stock 2 ="+stockUsage[z].stock)
+                          }
+                        }
+                        if(j==result2.length){
+                          console.log("Inside 9");
+                          resolve2('Done');
+                        }
+
+                      }); 
+                      promise2.then((resolve2)=>{
+                        console.log("Inside 10");
+                        if(resolve2=='Done'){
+                          console.log("Inside 11");
+                            let uptStr='CASE fkItemId';
+                            let itemIDs=[];
+                            let promise3 = new Promise((resolve3,reject3)=>{
+                              console.log("Inside 12");
+                            let p=0;
+                            console.log(stockUsage.length);
+                            for(p=0;p<stockUsage.length;p++){
+                              let myStr=" WHEN '"+stockUsage[p].itemId+"' THEN "+parseInt(stockUsage[p].stock);
+                              uptStr+=myStr;
+                              console.log(uptStr);
+                              itemIDs.push(stockUsage[p].itemId);
+                            }
+                            if(p==stockUsage.length){
+                              uptStr+=' END';
+                              console.log("Inside 13");
+                              console.log(uptStr);
+                              resolve3('Done');
+                            }
+                          });
+                          promise3.then((resolve3)=>{
+                              if(resolve3=='Done'){
+                                console.log("Inside 14 "+itemIDs);
+                                db.query( "update stock set stockQuantity = "+uptStr+" where fkItemId in (?) and fkBranchId = ? ",[itemIDs,myBranch], function(err3, rows3, fields3) {
+                                  if(rows3){
+
+                                    console.log("Inside 15");
+                                    saledetail.deleteSalesdetail(req.params.sid, function (err, rows) {
+                                      if (err) {
+                                        res.json(err);
+                                      } else {
+                                        res.json(rows);
+                                      }
+                                      });
+                                    console.log("Final Done");
+                                  }
+                                });
+                              }
+                          });
+
+                        }              
+              });
+            }  
+          });
+        }
+        });
+      }
+    });  
     }
     }); 
-  // db.query("select * from sale where saleId=?",[req.params.sid],function (err, result, fields) {
-  //     if(result){
-  //       // console.log("---"+ result[0]);
-  //       db.query("select * from salesdetails where fkSaleId = ?",[req.params.sid],function(err1,result1,fields1){
-  //         if(result1){
-  //           // console.log("---"+ result[0]);
-  //           let myBranch=result[0].fkBranchId;
-  //           let stockUsage=[];
-  //           for(let i=0;i<result1.length;i++){
-  //             stockUsage.push({
-  //               itemId:result1[i].fkItemId,
-  //               branchId:myBranch,
-  //               stock:result1[i].saleQuantity
-  //             });  
-  //           }
-  //           db.query("select * from stock where fkBranchId = ?",myBranch,function(err2,result2,fields2){
-  //             if(result2){
-  //               for(let j=0;j<result2.length;j++){
-  //                 for(let z=0;z<stockUsage.length;z++){
-  //                     if(result2[j].fkItemId==stockUsage[z].itemId){
-  //                       stockUsage[z].stock=result2[j].stockQuantity + stockUsage[z].stock; 
-  //                     }
-  //                 }
-  //               } 
-  //               let y=0;
-  //               while(  y < stockUsage.length  ) {
-  //                 let item = stockUsage[y].itemId;
-  //                 let branch=myBranch;
-  //                 let final=stockUsage[y].stock;
-  //                 //some manipulation of someArr[i]
-  //                 // (function(val1,val2,val3){
-  //                   let promise2 = new Promise((resolve2,reject3)=>{
-  //                   db.query( "update stock set stockQuantity = ? where fkItemId=? and fkBranchId = ? ",[val1,val2,val3], function(err3, rows3, fields3) {
-  //                       if ( err ) {
-  //                         console.log( err );
-  //                       } else {
-  //                         // console.log("ss "+final);
-  //                         resolve('Done');
-  //                         // output.push( rows[0].someVal );//push query output to this variable
-  //                       }
-  //                   });
-  //                 });
-  //                 promise2.then((resolve2)=>{
-  //                   if(resolve2=='Done'){
-  //                     y++;
-  //                   }
-  //                 });
-
-  //                 // })(final,item,branch);
-  //               }
-  //               if(y==stockUsage.length){
-  //                 res.json(rows);
-  //               }
-
-  //             }  
-  //           });
-  //         }  
-  //       });    
-  //     }
-  //       });
-  //     }
-  //   });
 });
 
 router.put("/:sid", function (req, res, next) {
