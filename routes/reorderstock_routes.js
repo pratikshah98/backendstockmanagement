ee7518177e9aab70e6f97424a0e29c2a745cd67c
Maulik Jadav <40969469+maulik9211@users.noreply.github.com>
2020-06-07 22:35:03 +0530

    Added cron job for Reordering stock

--- routes/reorderstock_routes.js
+++ routes/reorderstock_routes.js
@@ -1,92 +1,74 @@
-var reorder= require("../models/reorderstock_model");
+var reorder = require("../models/reorderstock_model");
 var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-router.get('/:bid',function(req,res,next){
- 
-    reorder.getitemforreorder(req.params.bid,function(err,rows){
-     
-    if(err)
-      {
+router.get("/:bid", function (req, res, next) {
+  reorder.getitemforreorder(req.params.bid, function (err, rows) {
+    if (err) {
       res.json(err);
-      }
-      else
-      { 
+    } else {
       //res.json(rows);
       //console.log(rows);
-      for(i=0;i<rows.length;i++)
-      {
-         console.log(rows[i]);
+      for (i = 0; i < rows.length; i++) {
+        // console.log(rows[i]);
         var transporter = nodemailer.createTransport({
-    
-            service: 'gmail',
-          
-            auth: {
-          
-              user: 'dhairyajariwala26@gmail.com',
-          
-              pass: 'abcdEfg@12'
-          
-            },
-            tls:{
-                rejectUnauthorized:false
-            }
-          
-          });
-          
-           
-          
-          var mailOptions = {
-          
-            from: 'dhairyajariwala26@gmail.com',
-          
-            to: rows[i].userEmailId,
-          
-            subject:'REMAINDER OF REORDER ITEM',
-          
-            //text:rows[i].itemId,
-            html:'<table style="border:1px solid black; padding:20px; margin:20px;"> '+
-            '<tr>'+
-            '<th>  ITEM NAME      <th>'+
-            '<th>  ITEM QUANTITY  <th>'+
-            '</tr>'+
+          service: "gmail",
 
-            '<tr>'+
-            '<th>'+ rows[i].name          +'</th>'+'&nbsp;&nbsp;&nbsp;'+
-            '<th>'+ rows[i].stockQuantity +'</th>'+
-            
-            '</tr>'+
-            '</table>'
-            
-          };
-          
-           
-          
-          transporter.sendMail(mailOptions, function(error, info){
-          
-            if (error) {
-          
-              console.log(error);
-          
-            } else {
-          
-              console.log('Email sent: ' + info.response);
-          
-            }
-          
-          });
-              
-           
-        
+          auth: {
+            user: "dhairyajariwala26@gmail.com",
+
+            pass: "abcdEfg@12",
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+        });
+
+        var mailOptions = {
+          from: "dhairyajariwala26@gmail.com",
+
+          to: rows[i].userEmailId,
+
+          subject: "REMAINDER OF REORDER ITEM",
+
+          //text:rows[i].itemId,
+          html:
+            '<table border style="border:1px solid black;"> ' +
+            "<tr>" +
+            "<th>  Item Name      </th>" +
+            "<th>  GSM  </th>" +
+            "<th>  Size  </th>" +
+            "<th>  Current Stock  </th>" +
+            "</tr>" +
+            "<tr>" +
+            "<td>" +
+            rows[i].name +
+            "</td>" +
+            "<td>" +
+            rows[i].gsm +
+            "</td>" +
+            "<td>" +
+            rows[i].size +
+            "</td>" +
+            "<td>" +
+            rows[i].stockQuantity +
+            "</td>" +
+            "</tr>" +
+            "</table>",
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
       }
-      
-          
-      }
+      res.send("Done");
+    }
+  });
 });
 
-});
-  
 module.exports = router;
-
-
